b685c1621cf21e347c3240833930bc242a431dca
EleenYang <yangsihan730@gmail.com>
2019-03-16 23:11:54 +0800

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -3,67 +3,67 @@ CREATE TYPE education AS ENUM ('other', 'high school', 'college', 'postgraduate'
 CREATE TYPE role AS ENUM ('finder','tasker');
 
 CREATE TABLE Accounts(
-	aid 			INTEGER PRIMARY KEY,
-  email 		VARCHAR(74) UNIQUE,
+  aid 		INTEGER PRIMARY KEY,
+  email 	VARCHAR(74) UNIQUE,
   username 	VARCHAR(74) NOT NULL,
   password 	VARCHAR(74) NOT NULL
 );
 
 CREATE TABLE Users(
-	aid 			INTEGER,
-  gen 			gender,
-  edu				education,
-  rid				INTEGER,
+  aid 		INTEGER,
+  gen 		gender,
+  edu		education,
+  rid		INTEGER,
   score     INTEGER,
-	PRIMARY KEY (aid),
-	FOREIGN KEY (aid) REFERENCES Accounts,
+  PRIMARY KEY (aid),
+  FOREIGN KEY (aid) REFERENCES Accounts,
   FOREIGN KEY (RID)	REFERENCES Regions
 );
 
 CREATE TABLE Specializes (
-	aid				INTEGER REFERENCES Users,
-  sname		  VARCHAR(74) REFERENCES Specializations,
-  PRIMARY KEY	(aid, eid)
+  aid		   INTEGER REFERENCES Users,
+  sname		   VARCHAR(74) REFERENCES Specializations,
+  PRIMARY KEY  (aid, eid)
 );
 
 CREATE TABLE Bids (
-  tid INTEGER REFERENCES Tasks,
+  tid       INTEGER REFERENCES Tasks,
   tasker_id INTEGER REFERENCES Users,
-  salary INTEGER NOT NULL,
-  PRIMARY KEY (tid, tasker_id)
+  salary    INTEGER NOT NULL,
+  PRIMARY   KEY (tid, tasker_id)
 );
 
 CREATE TABLE Specializations (
-  sname VARCHAR(74) PRIMARY KEY,
+  sname   VARCHAR(74) PRIMARY KEY,
   details VARCHAR(100),
   check (sname <> 'OTHER' OR details NOT NULL)
 );
 
 CREATE TABLE Admins(
-	aid 			   INTEGER PRIMARY KEY,
+  aid 			   INTEGER PRIMARY KEY,
   FOREIGN KEY(aid) REFERENCES Accounts
 );
 
 CREATE TABLE Tasks(
   tid INTEGER PRIMARY KEY,
-  title VARCHAR(74) UNIQUE,
-  sname VARCHAR(74) DEFAULT 'Unassigned' REFERENCES States,
-  rname VARCHAR(74) DEFAULT 'All' REFERENCES Regions,
-  cname VARCHAR(74) DEFAULT 'All' REFERENCES Classifications,
-  finder_id INTEGER REFERENCES Users(aid) NOT NULL,
-  salary INTEGER NOT NULL,
-  tasktime DATETIME NOT NULL,
+  title       VARCHAR(74) UNIQUE,
+  sname       VARCHAR(74) DEFAULT 'Unassigned' REFERENCES States,
+  rname       VARCHAR(74) DEFAULT 'All' REFERENCES Regions,
+  cname       VARCHAR(74) DEFAULT 'All' REFERENCES Classifications,
+  finder_id   INTEGER REFERENCES Users(aid) NOT NULL,
+  salary      INTEGER NOT NULL,
+  tasktime    DATETIME NOT NULL,
   description VARCHAR(200),
-  tasker_id INTEGER REFERENCES Users(aid)
+  tasker_id   INTEGER REFERENCES Users(aid)
   check (tasker_id <> finder_id)
 );
 
 CREATE TABLE Reviews(
-  tid INTEGER REFERENCES Tasks,
+  tid         INTEGER REFERENCES Tasks,
   reviewer_id INTEGER NOT NULL REFERENCES Users(aid),
   receiver_id INTEGER NOT NULL REFERENCES Users(aid),
   receiver_role role NOT NULL,
-  score INTEGER NOT NULL, 
+  score       INTEGER NOT NULL, 
   PRIMARY KEY (tid, reviewer_id, receiver_id),
   check(score >= 0 and score <= 5),
 );
