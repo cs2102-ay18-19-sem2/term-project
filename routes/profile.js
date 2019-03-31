78c836abf78484cee56a631ea7e7a3bef72fecf4
KongZijin <e0253754@u.nus.edu>
2019-03-31 11:37:51 +0800

    update git ignore

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('user_info', { title: 'Profile' });
+  res.render('profile', { title: 'Profile' });
 });
 
 module.exports = router;
